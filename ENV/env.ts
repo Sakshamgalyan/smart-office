2c8299c83c4e48166c4c129073eec60cdfbec19a
Saksham <saksham@paysecure.net>
2026-01-12 17:33:47 +0530

    ad

--- ENV/env.ts
+++ ENV/env.ts
@@ -1,1 +1,2 @@
-"DATABASE_URL = mongodb://localhost:27017/smartoffice"
+"DATABASE_URL = mongodb+srv://saksham4504:Saksham1@smartoffice.embq4z3.mongodb.net/?appName=smartOfficez"
+"GOOGLE_CLIENT_ID = 599432194652-oqbfe476f85bk38eeudejms1frj6qebq.apps.googleusercontent.com"
